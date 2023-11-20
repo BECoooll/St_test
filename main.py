229d2932e85db6e6a6d4b21341ebb966fc245877
BECooll <204708022@csu.edu.cn>
2023-11-20 21:55:49 +0800

    test

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    st.write('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
