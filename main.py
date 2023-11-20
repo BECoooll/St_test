bcd053ea81027a65ed5928855a3ddcce32d0c79f
BECooll <204708022@csu.edu.cn>
2023-11-20 22:29:46 +0800

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    st.write('PyCharm')
+    st.header('Welcome to Dijita AppStore')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
